6dfc20d27ef2a42876d81f9c9e0cc281a0b488d2
Beatriz Gonçalves de Souza <102838758+Beatrizgs1@users.noreply.github.com>
2023-09-25 20:53:41 -0300

    exercício 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -11,3 +11,21 @@ BEGIN
 END//
 
 DELIMITER ;
+
+
+
+-- 2. Livros por Categoria:
+
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID
+    JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END//
+
+DELIMITER ;
