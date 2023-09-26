e0475db98c08ba544fc72f4bbb5c35bdd418abe6
Beatriz Gonçalves de Souza <102838758+Beatrizgs1@users.noreply.github.com>
2023-09-25 21:03:22 -0300

    exercício 8

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -142,4 +142,20 @@ END//
 DELIMITER ;
 
 
+-- 8. Autor Mais Antigo:
+
+DELIMITER //
+
+CREATE PROCEDURE sp_AutorMaisAntigo(OUT nome_autor VARCHAR(255))
+BEGIN
+    SELECT CONCAT(Nome, ' ', Sobrenome) INTO nome_autor
+    FROM Autor
+    ORDER BY Data_Nascimento ASC
+    LIMIT 1;
+END//
+
+DELIMITER ;
+
+
+
 
