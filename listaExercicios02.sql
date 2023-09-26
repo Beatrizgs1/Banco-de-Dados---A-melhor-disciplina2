a9df52653de066dc2b98fa85e54b5c3cb422b9a2
Beatriz Gonçalves de Souza <102838758+Beatrizgs1@users.noreply.github.com>
2023-09-25 21:09:35 -0300

    exercício 10

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -157,5 +157,26 @@ END//
 DELIMITER ;
 
 
+-- 9. Documentação e Comentários:
+
+
+
+
+
+
+
+--10. Livros e Seus Autores:
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosESeusAutores()
+BEGIN
+    SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END //
+DELIMITER ;
+
+
 
 
