26fd68425bd99173cdcc0541caf88ead21044a08
Beatriz Gonçalves de Souza <102838758+Beatrizgs1@users.noreply.github.com>
2023-09-25 21:00:21 -0300

    exercício 6

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -83,4 +83,35 @@ END//
 DELIMITER ;
 
 
+-- 6. Extração de Títulos por Categoria:
+
+DELIMITER //
+
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE livro_titulo VARCHAR(255);
+    DECLARE cur CURSOR FOR
+        SELECT Livro.Titulo
+        FROM Livro
+        JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+        WHERE Categoria.Nome = categoria_nome;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO livro_titulo;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        SELECT livro_titulo;
+    END LOOP;
+
+    CLOSE cur;
+END//
+
+DELIMITER ;
+
+
 
